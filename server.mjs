a4ca243bb52becc28569c53021e2cc071985cb06
encrentsil <nyamekye.nesther.esther599@gmail.com>
2024-06-20 13:22:11 +0000

    File system in nodejs

--- server.mjs
+++ server.mjs
@@ -1,19 +1,35 @@
-import {createServer} from 'node:http';
-
+import { createServer } from 'node:http';
+import {unlink} from 'node:fs';
 //Define request handler
 
 //Create HTTP Server
 const server = createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>We have an HTTP Server</h1>');
+    console.log(req.url);
+    if (req.url.includes('create')) {
+        //Create File
+        writeFile('./fiifi.html', '<h1>Learning Node.js</h1>', () => {
+            //Return response
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('<h1>File added</h1>');
+        });
+    } else {
+        //Delete file    
+        unlink('', () => {
+            //Return response
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('<h1>File deleted</h1>');
+        });
     }
-    );
 
-// Listen for incoming request
-server.listen(3000, '127.0.0.1', () => {
-    console.log ('Server is running');
 });
 
-//Normal function vs Arrow function 
-function handleRequest () {}
-const handleRequest = () => {} 
+
+
+
+    // Listen for incoming request
+    server.listen(3000, '127.0.0.1', () => {
+        console.log('Server is running');
+    });
+
+    //Normal function vs Arrow function 
+    const handleRequest = () => { } 
